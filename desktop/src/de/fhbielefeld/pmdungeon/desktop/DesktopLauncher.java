7ada5bd434e63064031b426fcede89cde8bba819
Carsten Gips <cagix@fh-bielefeld.de>
2020-12-22 13:36:28 +0100

    switch to 10 fps to reduce cpu load

--- desktop/src/de/fhbielefeld/pmdungeon/desktop/DesktopLauncher.java
+++ desktop/src/de/fhbielefeld/pmdungeon/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("PM-Dungeon");
+        config.setForegroundFPS(10);
 		new Lwjgl3Application(new PMDungeon(), config);
 	}
 }
