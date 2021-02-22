5c00616c55608c12392f87f940b99ca1d3653017
Andre Matutat <amatutat@web.de>
2021-02-23 00:42:12 +0100

    add level

--- desktop/src/de/fhbielefeld/pmdungeon/desktop/DesktopLauncher.java
+++ desktop/src/de/fhbielefeld/pmdungeon/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setTitle("PM-Dungeon");
-        config.setForegroundFPS(10);
+        config.setForegroundFPS(30);
 		new Lwjgl3Application(new PMDungeon(), config);
 	}
 }
