7331b2b7cab2e5e9229542a03a0327d72a87b1d2
Andre Matutat <amatutat@web.de>
2021-03-09 14:44:50 +0100

    refactor MainGameController

--- core/src/de/fhbielefeld/pmdungeon/vorgaben/game/Controller/MainGameController.java
+++ core/src/de/fhbielefeld/pmdungeon/vorgaben/game/Controller/MainGameController.java
@@ -7,7 +7,9 @@ import de.fhbielefeld.pmdungeon.vorgaben.Monster;
 import de.fhbielefeld.pmdungeon.vorgaben.dungeonCreater.dungeonconverter.DungeonConverter;
 import de.fhbielefeld.pmdungeon.vorgaben.game.GameSetup;
 import de.fhbielefeld.pmdungeon.vorgaben.tools.DungeonCamera;
+
 import static com.badlogic.gdx.graphics.GL20.GL_COLOR_BUFFER_BIT;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -15,44 +17,48 @@ import java.lang.reflect.Method;
  * Controlls the game.
  * Setup for all important objects.
  * Contains gameloop.
- *
  */
 public class MainGameController extends ScreenAdapter {
 
-    public static final float VIRTUAL_HEIGHT = 5f;
-
-    // for setup reasons
+    /**
+     * Does some setup. Contains the global SpriteBatch.
+     */
     private GameSetup gameSetup;
 
-    //controlls all entitys
-    private  DungeonEntityController dungeonEntityController;
-    //the camera
-    private  DungeonCamera camera;
-    //controlls the level
-    private  DungeonWorldController dungeonWorldController;
+    /**
+     * Controls all entity's
+     */
+    private DungeonEntityController dungeonEntityController;
+    /**
+     * The viewport for the dungeon
+     */
+    private DungeonCamera camera;
+    /**
+     * Controls the level
+     */
+    private DungeonWorldController dungeonWorldController;
 
     /**
      * Creates new MainGameController
-     * @param gameSetup setup class with SpriteBatch
+     *
+     * @param gameSetup setup class
      */
     public MainGameController(final GameSetup gameSetup) {
         this.gameSetup = gameSetup;
         this.dungeonEntityController = new DungeonEntityController();
-
         // todo this.hud = new HeadUpDisplay(gameWorld);
 
+        setupWorldController();
 
-        //setup the WorldController. Uses reflection to manage stuff after a new level is loaded
         try {
-            Method functionToPass = MainGameController.class.getMethod("onLevelLoad");
-            Object [] arguments = new Object[0];
-            this.dungeonWorldController= new DungeonWorldController(gameSetup.getBatch(),functionToPass,this,arguments);
             dungeonWorldController.setupDungeon(new DungeonConverter().dungeonFromJson("core/assets/small_dungeon.json"));
-        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
-
         setupCamera();
+
         //your setup
         setup();
     }
@@ -62,10 +68,11 @@ public class MainGameController extends ScreenAdapter {
     //Here you can do stuff you want to do at the beginning of the game
     Hero h;
     Monster m;
-    public void setup(){
+
+    public void setup() {
         System.out.println("Game started");
-        h = new Hero(dungeonWorldController.getDungeon(),gameSetup.getBatch());
-        m = new Monster(dungeonWorldController.getDungeon(),gameSetup.getBatch());
+        h = new Hero(dungeonWorldController.getDungeon(), gameSetup.getBatch());
+        m = new Monster(dungeonWorldController.getDungeon(), gameSetup.getBatch());
 
         camera.follow(m);
         dungeonEntityController.addEntity(h);
@@ -73,20 +80,22 @@ public class MainGameController extends ScreenAdapter {
     }
 
     //things you want to do at the begin of every frame
-    public void beginFrame(){
+    public void beginFrame() {
 
 
     }
+
     //things you want to do at the end of every frame
-    public void endFrame(){
-            //trigger next stage
-            if (dungeonWorldController.checkForTrigger(h.getPosition())) dungeonWorldController.triggerNextStage();
+    public void endFrame() {
+        //trigger next stage
+        if (dungeonWorldController.checkForTrigger(h.getPosition())) dungeonWorldController.triggerNextStage();
     }
+
     /**
      * This methode will be called by the DungeonWorldController every time a new level is loaded.
-     * Usefull to place new monster and items and remove old ones from the entityController
+     * Useful to place new monster and items and remove old ones from the entityController
      */
-    public void onLevelLoad(){
+    public void onLevelLoad() {
         System.out.println("Level loaded");
         h.updateLevel(dungeonWorldController.getDungeon());
         m.updateLevel(dungeonWorldController.getDungeon());
@@ -98,8 +107,8 @@ public class MainGameController extends ScreenAdapter {
 
     /**
      * Main gameloop.
-     *
-     * @param delta Time since last loop.
+     * Redraws the dungeon and calls all the update methods.
+     * @param delta Time since last loop. (since the PM-Dungeon is frame based, this isn't very usefull)
      */
     @Override
     public void render(float delta) {
@@ -123,6 +132,8 @@ public class MainGameController extends ScreenAdapter {
 
         //updates camera
         gameSetup.getBatch().setProjectionMatrix(camera.combined);
+
+        //updates camera position
         camera.update();
 
         endFrame();
@@ -136,10 +147,24 @@ public class MainGameController extends ScreenAdapter {
     private void setupCamera() {
         camera = new DungeonCamera(null);
         camera.position.set(0, 0, 0);
-        camera.zoom +=3;
+        camera.zoom += 3;
         camera.update();
     }
 
+    /**
+     * Setting up the WorldController.
+     */
+    private void setupWorldController() {
+        try {
+            //this method will be called every time a new level gets load
+            Method functionToPass = MainGameController.class.getMethod("onLevelLoad");
+            //if you need parameter four your method, add them here
+            Object[] arguments = new Object[0];
+            this.dungeonWorldController = new DungeonWorldController(gameSetup.getBatch(), functionToPass, this, arguments);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }
 
     /**
      * Resizing the camera according to the size of the window.
@@ -149,6 +174,8 @@ public class MainGameController extends ScreenAdapter {
      */
     @Override
     public void resize(int width, int height) {
+        //was static but only used here
+        float VIRTUAL_HEIGHT = 5f;
         camera.setToOrtho(false, VIRTUAL_HEIGHT * width / (float) height, VIRTUAL_HEIGHT);
         gameSetup.getBatch().setProjectionMatrix(camera.combined);
     }
