089ff170d5bd237d53823ca8570b60b2b69f993d
xGRKE <dmndnlrstz@gmail.com>
2021-06-05 03:14:42 +0200

    set test structure

---
+++ core/test/de/fhbielefeld/pmdungeon/desktop/tools/test.java
@@ -0,0 +1,44 @@
+package de.fhbielefeld.pmdungeon.desktop.tools;
+
+import org.junit.jupiter.api.*;
+
+public class test {
+
+    @BeforeAll
+    static void setup(){
+        System.out.println("@BeforeAll executed");
+    }
+
+    @BeforeEach
+    void setupThis(){
+        System.out.println("@BeforeEach executed");
+    }
+
+    @Tag("DEV")
+    @Test
+    void testCalcOne()
+    {
+        System.out.println("======TEST ONE EXECUTED=======");
+        Assertions.assertEquals( 4 , 2+2);
+    }
+
+    @Tag("PROD")
+    @Disabled
+    @Test
+    void testCalcTwo()
+    {
+        System.out.println("======TEST TWO EXECUTED=======");
+        Assertions.assertEquals( 6 , 8-2);
+    }
+
+    @AfterEach
+    void tearThis(){
+        System.out.println("@AfterEach executed");
+    }
+
+    @AfterAll
+    static void tear(){
+        System.out.println("@AfterAll executed");
+    }
+
+}
