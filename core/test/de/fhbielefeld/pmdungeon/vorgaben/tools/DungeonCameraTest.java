421e782e022378fe735230e077f8eb8f4c253120
xGRKE <dmndnlrstz@gmail.com>
2021-06-22 23:13:55 +0200

    completed testing DungeonCamera

--- core/test/de/fhbielefeld/pmdungeon/vorgaben/tools/DungeonCameraTest.java
+++ core/test/de/fhbielefeld/pmdungeon/vorgaben/tools/DungeonCameraTest.java
@@ -3,6 +3,7 @@ package de.fhbielefeld.pmdungeon.vorgaben.tools;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.math.Vector3;
 import de.fhbielefeld.pmdungeon.vorgaben.interfaces.IDrawable;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,12 +13,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @DisplayName("TestCase: DungeonCamera")
 public class DungeonCameraTest {
     private HeadlessApplication headlessApplication;
-    private final float vw = 50f;
-    private final float vh = 50f;
+    private final float vw = 42;
+    private final float vh = 69;
 
     @BeforeEach
     void setUp() {
@@ -34,6 +36,7 @@ public class DungeonCameraTest {
         headlessApplication.exit();
     }
 
+    //-----------constructor--------------
     @DisplayName("normal constructor")
     @Test
     void normalConstructor() {
@@ -53,6 +56,77 @@ public class DungeonCameraTest {
         assertEquals(camera.viewportWidth, vw);
         assertEquals(camera.viewportHeight, vh);
         assertNull(camera.getFollowedObject());
+    }
+
+    //-----------removeEntity+getFollowed--------------
+    @DisplayName("camera follow + get Followed")
+    @Test
+    void followAndGetFollowed() {
+        DungeonCamera camera = new DungeonCamera(null, vw, vh);
+        IDrawable toFollow = mock(IDrawable.class);
+
+        camera.follow(toFollow);
+
+        assertEquals(camera.getFollowedObject(), toFollow);
+    }
+
+    //-----------setFocusPoint--------------
+    @DisplayName("set focus point")
+    @Test
+    void setFocusPoint() {
+        DungeonCamera camera = new DungeonCamera(null, vw, vh);
+        Point point = new Point(vw, vh);
+
+        camera.setFocusPoint(point);
+
+        assertNull(camera.getFollowedObject());
+        // assertEquals(camera.focusPoint); ist private --> maybe DungeonCamera mocken?
+    }
+
+    //-----------update--------------
+    @DisplayName("update camera with all params")
+    @Test
+    void updateWithParams() {
+        // define mock with return value
+        IDrawable drawable = mock(IDrawable.class);
+        when(drawable.getPosition()).thenReturn(new Point(-vw, -vh));
+        DungeonCamera camera = new DungeonCamera(drawable, 0, 0);
+
+        camera.update();
+
+        assertEquals(camera.position.x, -vw);
+        assertEquals(camera.position.y, -vh);
+        assertEquals(camera.position.z, 0); // z immer 0
+    }
+
+    @DisplayName("update camera follows, focus being null")
+    @Test
+    void updateWitAllNull() {
+        DungeonCamera camera = new DungeonCamera(null, vw, vh);
+        assertNull(camera.getFollowedObject());
+        assertEquals(camera.position, new Vector3(0,0,0));
+
+        camera.update();
+
+        assertEquals(camera.position.x, 0);
+        assertEquals(camera.position.y, 0);
+        assertEquals(camera.position.z, 0);
+    }
+
+    @DisplayName("update camera with follows being null and focus not being null")
+    @Test
+    void updateWithFollowNullFocusNotNull() {
+        DungeonCamera camera = new DungeonCamera(null, vw, vh);
+        camera.setFocusPoint(new Point(vw, vh));
+        assertEquals(camera.position.x, 0);
+        assertEquals(camera.position.y, 0);
+        assertEquals(camera.position.z, 0);
+
+        camera.update();
+
+        assertEquals(camera.position.x, vw);
+        assertEquals(camera.position.y, vh);
+        assertEquals(camera.position.z, 0);
     }
 
 }
