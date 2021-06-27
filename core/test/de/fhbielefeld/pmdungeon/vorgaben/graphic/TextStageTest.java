acb9ac5499ede0fd7f3e5983583aa9a55a0a1e32
xGRKE <dmndnlrstz@gmail.com>
2021-06-27 19:38:24 +0200

    completed TextStageTest

--- core/test/de/fhbielefeld/pmdungeon/vorgaben/graphic/TextStageTest.java
+++ core/test/de/fhbielefeld/pmdungeon/vorgaben/graphic/TextStageTest.java
@@ -12,17 +12,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.UncheckedIOException;
 import java.lang.reflect.Field;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 @DisplayName("TestCase: TextStage")
 public class TextStageTest {
     protected HeadlessApplication headlessApplication;
+    private final String text = "Sample";
+    private final String fontPath = "./assets/Asdonuts.ttf";
+    private final Color color = Color.BLACK;
+    private final int ints = 0;
 
     @BeforeEach
     void setUp() {
@@ -33,56 +35,60 @@ public class TextStageTest {
             public void create() { }
         }, config);
 
-        // mock OpenGL context, because headless backend does not have it
+        // mock OpenGL context, because headless backend does not have it, but is needed
         Gdx.gl = mock(GL20.class);
     }
 
     //-----------constructor--------------
-    @DisplayName("normal constructor")
+    @DisplayName("constructor: normal")
     @Test
-    void constructor() throws NoSuchFieldException, IllegalAccessException {
-        SpriteBatch pls = mock(SpriteBatch.class);
-        TextStage textStage = new TextStage(pls);
-
+    void testConstructor() throws NoSuchFieldException, IllegalAccessException {
         Field labelsField = TextStage.class.getDeclaredField("labels");
         labelsField.setAccessible(true);
-        assertTrue(((List<Label>) labelsField.get(textStage)).isEmpty());
+        SpriteBatch spriteBatch = mock(SpriteBatch.class);
+        TextStage textStage = new TextStage(spriteBatch);
 
-        // TODO: maybe ScreenViewport und Stage dimensions testen
+        assertNotNull(textStage, "TextStage must exist");
+        assertTrue(((List<Label>) labelsField.get(textStage)).isEmpty(), "label List should be empty");
     }
 
     //-----------drawText--------------
     @DisplayName("draw text")
     @Test
-    void drawTect() throws NoSuchFieldException, IllegalAccessException {
-        SpriteBatch pls = mock(SpriteBatch.class);
-        TextStage textStage = new TextStage(pls);
+    void testDrawText() throws NoSuchFieldException, IllegalAccessException {
         Field labelsField = TextStage.class.getDeclaredField("labels");
         labelsField.setAccessible(true);
+        TextStage textStage = new TextStage(mock(SpriteBatch.class));
         assertTrue(((List<Label>) labelsField.get(textStage)).isEmpty());
 
-        Label label = textStage.drawText("Sample", "./assets/Asdonuts.ttf", Color.BLACK, 0,0,0,0,0);
+        Label label = textStage.drawText(text, fontPath, color, ints, ints, ints, ints, ints);
 
-        assertTrue(((List<Label>) labelsField.get(textStage)).contains(label));
+        assertTrue(((List<Label>) labelsField.get(textStage)).contains(label), "label should be in list");
+        assertEquals(label.getText().toString(), text, "label text must match");
+        assertEquals(label.getHeight(), ints, "label height must match");
+        assertEquals(label.getWidth(), ints, "label width must match");
+        assertEquals(label.getX(), ints, "label x position must match");
+        assertEquals(label.getY(), ints, "label y position must match");
     }
 
     //-----------removeText--------------
     @DisplayName("remove text")
     @Test
-    void removeText() throws NoSuchFieldException, IllegalAccessException {
-        SpriteBatch pls = mock(SpriteBatch.class);
-        TextStage textStage = new TextStage(pls);
+    void testRemoveText() throws NoSuchFieldException, IllegalAccessException {
+        TextStage textStage = new TextStage(mock(SpriteBatch.class));
         Field labelsField = TextStage.class.getDeclaredField("labels");
         labelsField.setAccessible(true);
-        Label label = textStage.drawText("Sample", "./assets/Asdonuts.ttf", Color.BLACK, 0,0,0,0,0);
-        assertTrue(((List<Label>) labelsField.get(textStage)).contains(label));
+        Label label1 = textStage.drawText(text, fontPath, Color.BLACK, ints, ints, ints, ints, ints);
+        Label label2 = textStage.drawText(text, fontPath, Color.BLACK, ints, ints, ints, ints, ints);
+        assertEquals(((List<Label>) labelsField.get(textStage)).size(), 2);
 
-        textStage.removeText(label);
+        textStage.removeText(label1);
 
-        assertFalse(((List<Label>) labelsField.get(textStage)).contains(label));
+        assertFalse(((List<Label>) labelsField.get(textStage)).contains(label1), "label should have been removed from list");
+        assertTrue(((List<Label>) labelsField.get(textStage)).contains(label2), "label should remain in list");
     }
 
-    // draw() - nicht testen, da nur LibGDX Methoden aufgerufen werden
-    // clear() - nicht testen, da nur LibGDX Methoden aufgerufen werden
+    // draw() - nicht testen, da nur LibGDX Methoden von Stage aufgerufen werden
+    // clear() - nicht testen, da nur LibGDX Methoden von Stage aufgerufen werden
 
 }
